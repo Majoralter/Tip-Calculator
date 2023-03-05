0fdb56630a30cebff66b4b626b9a7b2aeee79dbe
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-03-05 17:31:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ resetBtn.classList.add("active")
     resetBtn.classList.remove("active")
 }
 
+if(bill === "" || noOfPeople === ""){
+    amountPerPerson.textContent = "$0.00"
+}
+
 document.getElementById("custom-tip").onkeyup = () =>{
     let tipPrice = document.getElementById("tip-amount")
        let tipPercent = document.getElementById("custom-tip").value
